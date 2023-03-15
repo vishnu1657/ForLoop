92d1ce5bdd1f47b0fcff1f0412a2d95a3db964c3
HP <HP@DESKTOP-4Q8V1JF>
2023-03-15 13:01:11 +0530

    Add project files.

---
+++ ForLoop/Program.cs
@@ -0,0 +1,23 @@
+﻿using ForLoop;
+
+
+public class Program
+{
+    public static void Main()
+    {
+        //Pattern.StaticFunction1();
+
+        Class1 patternObject = new Class1();
+        // patternObject.NonStaticFunction2();
+
+        patternObject.StarPattern1();
+        patternObject.StarPattern2();
+        patternObject.StarPattern3();
+        patternObject.StarPattern4();
+        patternObject.StarPattern5();
+        patternObject.StarPattern6();
+
+
+
+    }
+}   
