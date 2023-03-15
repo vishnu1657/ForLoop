7db659a28f570a25e8467f1a2033610b1fe54228
HP <HP@DESKTOP-4Q8V1JF>
2023-03-15 13:03:15 +0530

    For loop star pattern

--- ForLoop/Program.cs
+++ ForLoop/Program.cs
@@ -5,11 +5,10 @@ public class Program
 {
     public static void Main()
     {
-        //Pattern.StaticFunction1();
+        
 
         Class1 patternObject = new Class1();
-        // patternObject.NonStaticFunction2();
-
+        
         patternObject.StarPattern1();
         patternObject.StarPattern2();
         patternObject.StarPattern3();
